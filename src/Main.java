9542c8be94489d3f8d1139544d24f67e4a36b036
INizetich <ignacionizetich@gmail.com>
2024-11-13 15:47:58 -0300

    FIX chanchito

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 
         public class Main {
             ///Negrito rriko
+            ///blanquito feo
 
             ///GORDO PUTO
             public static void main(String[] args) {

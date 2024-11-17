5e938824e24cb455e1a4eede92cddf5ea4b43d2f
Lautaro <lautaarschak@gmail.com>
2024-11-17 12:46:06 -0300

    Modificaciones menu

--- src/UI/MenuClientes.java
+++ src/UI/MenuClientes.java
@@ -37,7 +37,7 @@ public class MenuClientes {
             System.out.println("1️⃣ Hacer una reserva 🛫");
             System.out.println("2️⃣ Ingresar a tiendas 🏬");
             System.out.println("3️⃣ ATM 💰");
-            System.out.println("4️⃣ Salir 🚪");
+            System.out.println("4️⃣ Salir 👋");
             opcionCliente = scanner.nextInt();
             scanner.nextLine(); // Limpiar el buffer de entrada
 
@@ -64,8 +64,8 @@ public class MenuClientes {
                                     e.printStackTrace();
                                 } finally {
                                     System.out.println("==================================");
-                                    System.out.println("🔄 ¿Desea hacer otra reserva? ✈️");
-                                    System.out.println("👉 (s: sí / n: no)");
+                                    System.out.println(" ➡️ ¿Desea hacer otra reserva? ✈️");
+                                    System.out.println("👉 (s: ✔️ / n: ❌)");
                                     System.out.println("==================================");
                                     opcionString = scanner.nextLine().trim().toLowerCase();
                                 }
@@ -87,7 +87,7 @@ public class MenuClientes {
                                     CheckIn.mostrarReserva(dni, sistemaReserva);
                                     System.out.println("==================================");
                                     System.out.println("🔄 ¿Desea consultar otra reserva? 🤔");
-                                    System.out.println("👉 (s: sí / n: no)");
+                                    System.out.println("👉 (s: ✔️ / n: ❌)");
                                     System.out.println("==================================");
                                     opcionConsulta = scanner.nextLine().trim().toLowerCase();
                                 } catch (dniNoEncontradoException e) {
@@ -110,11 +110,10 @@ public class MenuClientes {
 
                             do {
                                 try {
-                                    // Preguntar si desea consultar el boleto de avión solo la primera vez
                                     if (!generarBoleto.equals("s")) {
                                         System.out.println("==============================================");
                                         System.out.println("🌟 ¿Desea consultar su boleto de avión? 🌟");
-                                        System.out.println("📝 Ingrese 's' para Sí o 'n' para No.");
+                                        System.out.println("📝 Ingrese 's' para ✔️ o 'n' para ❌.");
                                         opcionString = scanner.nextLine().trim().toLowerCase();
                                     }
 
@@ -131,22 +130,22 @@ public class MenuClientes {
 
                                     // Preguntar si desea generar otro boleto
                                     System.out.println("¿Desea generar otro boleto de avión? 🤔");
-                                    System.out.println("📝 Ingrese 's' para Sí o 'n' para No.");
+                                    System.out.println("📝 Ingrese 's' para ✔️ o 'n' para ❌.");
                                     generarBoleto = scanner.nextLine().trim().toLowerCase();
                                 } catch (ReservaInexistenteException e) {
                                     // Mostrar animación de carga
                                     Utilidades.Utilities.mostrarCargando();
 
                                     // Mensaje si no se encuentra la reserva
-                                    System.out.println("❌ No se encontró una reserva asociada al DNI ingresado.");
-                                    System.out.println("🛑 Por favor, verifique la información e intente nuevamente.");
-                                    System.out.println("\nPresione Enter para seguir con el programa...");
-                                    scanner.nextLine();  // Espera a que el usuario presione Enter
+                                    System.out.println("❌ No se encontró una reserva asociada al DNI ingresado. ❌");
+                                    System.out.println("🛑 Por favor, verifique la información e intente nuevamente. 🛑");
+                                    System.out.println("\nPresione Enter para seguir con el programa... 👈");
+                                    scanner.nextLine();
 
-                                    mostrarMensajeFinal = false;  // Evitar mostrar el mensaje final
-                                    break; // Salir del ciclo si ocurre la excepción
+                                    mostrarMensajeFinal = false;
+                                    break;
                                 }
-                            } while (generarBoleto.equals("s")); // Repetir si el usuario ingresa 's'
+                            } while (generarBoleto.equals("s"));
 
                             // Mostrar mensaje final solo si no ocurrió una excepción
                             if (mostrarMensajeFinal) {
@@ -160,32 +159,32 @@ public class MenuClientes {
                         // Agregar más casos si es necesario
                     }
                     // Esperar que el usuario presione Enter para volver al menú principal
-                    System.out.println("Presione Enter para volver al menú principal...");
+                    System.out.println("🔄Presione Enter para volver al menú principal...🔄");
                     scanner.nextLine();
                     break;
 
                 case 2:
                     // Implementar el caso 2 (Ingresar a tiendas)
                     System.out.println("Usted ha elegido ingresar a tiendas. 🏬");
-                    System.out.println("Presione Enter para volver al menú principal...");
-                    scanner.nextLine();  // Esperar que el usuario presione Enter
+                    System.out.println("🔄Presione Enter para volver al menú principal...🔄");
+                    scanner.nextLine();
                     break;
 
                 case 3:
                     // Implementar el caso 3 (ATM)
                     System.out.println("Usted ha elegido ATM. 💰");
-                    System.out.println("Presione Enter para volver al menú principal...");
-                    scanner.nextLine();  // Esperar que el usuario presione Enter
+                    System.out.println("🔄Presione Enter para volver al menú principal...🔄");
+                    scanner.nextLine();
                     break;
 
                 case 4:
-                    System.out.println("🚪 Gracias por utilizar nuestros servicios. ¡Hasta luego!");
+                    System.out.println("🚪 Gracias por utilizar nuestros servicios. ¡Hasta luego! 🚪");
                     break;
 
                 default:
-                    System.out.println("Opción no válida. Por favor, intente nuevamente.");
+                    System.out.println("Opción no válida. Por favor, intente nuevamente. 😤😤");
                     break;
             }
-        } while (opcionCliente != 4); // Salir cuando se elija la opción 4
+        } while (opcionCliente != 4);
     }
 }

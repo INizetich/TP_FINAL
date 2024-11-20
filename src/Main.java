29329e10ebf11cd90bf9a50ceda28422e1fad3ba
INizetich <ignacionizetich@gmail.com>
2024-11-20 03:01:13 -0300

    FEAT PRE FINAL?

--- src/Main.java
+++ src/Main.java
@@ -10,3 +10,14 @@ public class Main {
         Menu.Menu();
     }
 }
+
+
+/*
+* la verdad que estamos muy agradecidos por todo el aprendizaje que nos presento este proyecto, queremos agradecerle a carolina archuby por su paciencia y su templanza a la hora
+* de enseñar la materia y por aceptarnos este trabajo aunque no hayamos seguido al 100% las exigencias de la catedra.
+*
+*
+*
+*
+*
+* */

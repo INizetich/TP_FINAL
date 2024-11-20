6518222422178bbe1ae942a53fa931b6b81847e5
INizetich <ignacionizetich@gmail.com>
2024-11-20 03:03:36 -0300

    FEAT FINAL!

--- src/Main.java
+++ src/Main.java
@@ -16,8 +16,11 @@ public class Main {
 * la verdad que estamos muy agradecidos por todo el aprendizaje que nos presento este proyecto, queremos agradecerle a carolina archuby por su paciencia y su templanza a la hora
 * de enseñar la materia y por aceptarnos este trabajo aunque no hayamos seguido al 100% las exigencias de la catedra.
 *
+*a pesar de eso espero que te guste este trabajo en el cual pusimos mucho esfuerzo y dedicacion a la hora de hacerlo
+*
+*P.D: tratamos de manejar lo mejor posible los bugs, espero que no se rompa <3
 *
 *
-*
+* Created and developed by: Nizetich Ignacio, Arschak Lautaro, Shaffer Arian, Figueroa Rufino.
 *
 * */
